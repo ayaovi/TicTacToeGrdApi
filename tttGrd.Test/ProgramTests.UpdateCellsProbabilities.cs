4bc9231b3ee5b2d91d0272b0e8c705318ee96696
ayaovi <djayespoiro@gmail.com>
2017-09-04 17:08:18 +0200

    chore: non-working code.

--- tttGrd.Test/ProgramTests.UpdateCellsProbabilities.cs
+++ tttGrd.Test/ProgramTests.UpdateCellsProbabilities.cs
@@ -37,28 +37,17 @@ namespace tttGrd.Test
 
       var move = (Grid: 0, Cell: 4);
 
-      var expected1 = new[]
-      {
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f },
-        new[] { 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f }
-      };
+      var expected1 = GetDefaultCellsProbabilitiesForTest();
       Enumerable.Range(0, 9)
-        .ToList()
-        .ForEach(x => expected1[0][x] = 1f / 3f);
+                .ToList()
+                .ForEach(x => expected1[0][x] = 1f / 3f);
 
       expected1[0][4] = 0.0f; /* middle one should not be taken again. */
 
       var expected2 = expected1.Copy(x => x.Select(x1 => x1).ToArray()).ToArray();
       Enumerable.Range(1, 8)
-        .ToList()
-        .ForEach(x => expected2[x][0] -= 1f / 9f);
+                .ToList()
+                .ForEach(x => expected2[x][0] -= 1f / 9f);  /* if you can help it, avoid sending the opponent back in grid 0 until you have played there yourself. */
 
       //Act
       var cells1 = Program.UpdateCellsProbabilities(gamer1.CellProbabilities, gamer1.GameState, move, gamer1.Indicator);
@@ -67,6 +56,75 @@ namespace tttGrd.Test
       //Assert
       cells1.ShouldAllBeEquivalentTo(expected1);
       cells2.ShouldAllBeEquivalentTo(expected2);
+    }
+
+    [Test]
+    public void UpdateCellsProbabilities_GivenPreviousZeroZero_ExpectUpdate()
+    {
+      //Arrange
+      var gamer1 = new Gamer
+      {
+        Indicator = Field.X,
+        Name = "Gamer_1",
+        GameState = new State(new[]{
+          "...|.x.|...", "...|...|...", "...|...|...",
+          "...|...|...", "...|.o.|...", "...|...|...",
+          "...|...|...", "...|...|...", "...|...|..."
+        }),
+        Oponent = Field.O
+      };
+      gamer1.CellProbabilities = Program.UpdateCellsProbabilities(gamer1.CellProbabilities, gamer1.GameState, (0, 4), gamer1.Indicator);
+
+      var gamer2 = new Gamer
+      {
+        Indicator = Field.O,
+        Name = "Gamer_2",
+        GameState = new State(new[]{
+          "...|.x.|...", "...|...|...", "...|...|...",
+          "...|...|...", "...|.o.|...", "...|...|...",
+          "...|...|...", "...|...|...", "...|...|..."
+        }),
+        Oponent = Field.X
+      };
+      gamer2.CellProbabilities = Program.UpdateCellsProbabilities(gamer2.CellProbabilities, gamer2.GameState, (0, 0), gamer2.Indicator);
+
+      var move = (Grid: 4, Cell: 4);
+
+      var expected2 = GetDefaultCellsProbabilitiesForTest();
+      Enumerable.Range(0, 9)
+                .ToList()
+                .ForEach(x => expected2[4][x] += 2f / 9f);
+      expected2[4][4] = 0.0f; /* middle one should not be taken again. */
+
+      var expected1 = gamer1.CellProbabilities.Copy(x => x.Select(x1 => x1).ToArray()).ToArray();
+      Enumerable.Range(1, 8)
+                .ToList()
+                .ForEach(x => expected1[x][0] -= 1f / 9f);  /* if you can help it, avoid sending the opponent back in grid 0 until you have played there yourself. */
+      expected1[4][4] = 0.0f; /* middle one should not be taken again. */
+
+      //Act
+      var cells1 = Program.UpdateCellsProbabilities(gamer1.CellProbabilities, gamer1.GameState, move, gamer1.Indicator);
+      var cells2 = Program.UpdateCellsProbabilities(gamer2.CellProbabilities, gamer2.GameState, move, gamer2.Indicator);
+
+      //Assert
+      cells1.ShouldAllBeEquivalentTo(expected1);
+      cells2.ShouldAllBeEquivalentTo(expected2);
+    }
+
+    private static float[][] GetDefaultCellsProbabilitiesForTest()
+    {
+      return new[]
+      {
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f},
+        new[] {1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f, 1f, 1f / 9f, 1f / 9f, 1f / 9f, 1f / 9f}
+      };
     }
   }
 }
