ff2e5bda71c68a36fc2f874fd8489cd3a82972a5
Ayaovi <djayespoiro@gmail.com>
2017-09-15 07:24:42 +0200

    chore: I have no idea what I am doing in the currently failing test.

--- tttGrd.Test/ProgramTests.UpdateCellsProbabilities.cs
+++ tttGrd.Test/ProgramTests.UpdateCellsProbabilities.cs
@@ -42,7 +42,7 @@ namespace tttGrd.Test
                 .ToList()
                 .ForEach(x => expected1[0][x] = 1f / 3f);
 
-      expected1[0][4] = 0.0f; /* middle one should not be taken again. */
+      expected1[0][4] = 0.0f; /* cell 4 in grid 0 cannot be played again. */
 
       var expected2 = expected1.Copy(x => x.Select(x1 => x1).ToArray()).ToArray();
       Enumerable.Range(1, 8)
@@ -62,6 +62,11 @@ namespace tttGrd.Test
     public void UpdateCellsProbabilities_GivenPreviousZeroZero_ExpectUpdate()
     {
       //Arrange
+      /*
+       the play was going as follow:
+       - gamer1 made move (0,4)
+       - gamer2 replied with move (4,4)
+       */
       var gamer1 = new Gamer
       {
         Indicator = Field.X,
@@ -90,18 +95,18 @@ namespace tttGrd.Test
 
       var move = (Grid: 4, Cell: 4);
 
+      var expected1 = gamer1.CellProbabilities.Copy(x => x.Select(x1 => x1).ToArray()).ToArray();
+      Enumerable.Range(1, 9)
+                .ToList()
+                .ForEach(x => expected1[x][0] -= 1f / 9f);  /* if you can help it, avoid sending the opponent back in grid 0 until you have played there yourself. */
+      expected1[4][4] = 0.0f; /* middle one should not be taken again. */
+
       var expected2 = GetDefaultCellsProbabilitiesForTest();
       Enumerable.Range(0, 9)
                 .ToList()
                 .ForEach(x => expected2[4][x] += 2f / 9f);
       expected2[4][4] = 0.0f; /* middle one should not be taken again. */
-
-      var expected1 = gamer1.CellProbabilities.Copy(x => x.Select(x1 => x1).ToArray()).ToArray();
-      Enumerable.Range(1, 8)
-                .ToList()
-                .ForEach(x => expected1[x][0] -= 1f / 9f);  /* if you can help it, avoid sending the opponent back in grid 0 until you have played there yourself. */
-      expected1[4][4] = 0.0f; /* middle one should not be taken again. */
-
+      
       //Act
       var cells1 = Program.UpdateCellsProbabilities(gamer1.CellProbabilities, gamer1.GameState, move, gamer1.Indicator);
       var cells2 = Program.UpdateCellsProbabilities(gamer2.CellProbabilities, gamer2.GameState, move, gamer2.Indicator);
