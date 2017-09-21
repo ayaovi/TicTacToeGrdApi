e72a91b00be164c4905e6d490e4ee2493c51cf1c
Ayaovi <djayespoiro@gmail.com>
2017-09-22 01:00:17 +0200

    Adding SignalR.

---
+++ src/tttGrd.Api/Hubs/GameHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace tttGrd.Api.Hubs
+{
+  public class GameHub : Hub
+  {
+    public Task JoinAgniKai(string agniKaiTicket)
+    {
+      return Groups.Add(Context.ConnectionId, agniKaiTicket);
+    }
+
+    public Task LeaveAgniKai(string agniKaiTicket)
+    {
+      return Groups.Remove(Context.ConnectionId, agniKaiTicket);
+    }
+  }
+}
