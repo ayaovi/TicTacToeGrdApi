d8ee2ae702a5d9278b15cf191eb631b8fa36b15a
ayaovi <djayespoiro@gmail.com>
2017-10-01 00:41:09 +0200

    feat: Add dummy grid.

--- src/tttGrd.Api/Scripts/game.js
+++ src/tttGrd.Api/Scripts/game.js
@@ -8,6 +8,7 @@
   var hub = $.connection.gameHub; // create a proxy to signalr hub on web server
 
   app.controller("myCtrl", ["$http", "$scope", function ($http, $scope) {
+    $scope.cellIds = [];
     $scope.getAgniKaiTicket = function () {
       $http.get(agniKaiUri + "/initiate")
         .success(function (data, status) {
@@ -21,7 +22,27 @@
           $scope.errorToSearch = errorMessage(data, status);
         });
     };
+
+    $scope.recordMove = function (cellId) {
+      var move = $scope.extractMove(cellId);
+      var encodeMove = $("<div />").text(move).html();
+      $("#playerOnline").append("<li>" + encodeMove + "</li>");
+    };
+
+    $scope.extractMove = function (cellId) {
+      var n = Math.floor(cellId / 27);
+      var r = cellId % 27;
+      var x = r % 3;
+      var grid = (n * 3) + 3 - x % 3;
+      var cell = r % 3;
+      return  cellId + ": " + "(" + grid + ", " + cell + ")";
+    };
+
     $("#gamerName").val(prompt("Enter your name:", ""));
+    for (var i = 0; i < 81; i++) {
+      $scope.cellIds.push(i);
+    }
+
     $.connection.hub.start(); // connect to signalr hub
   }]);
 })();
