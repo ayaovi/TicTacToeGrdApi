74e3484425385cdb8cd26b6a440609abe10cb732
Ayaovi <djayespoiro@gmail.com>
2017-11-06 19:31:30 +0200

    Add agnikai controller test.

---
+++ src/tttGrd.Api.Tests/Controllers/AgniKaiControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using tttGrd.Api.Controllers;
+using tttGrd.Api.Models;
+using tttGrd.Api.Persistence;
+
+namespace tttGrd.Api.Tests.Controllers
+{
+  [TestFixture]
+  class AgniKaiControllerTests
+  {
+    [Test]
+    public async Task Initiate_GivenTicket_ExpectAICreated()
+    {
+      //Arrange
+      var vault = Substitute.For<IVault>();
+      vault.AddAgniKaiTicket(Arg.Any<string>()).Returns(Task.CompletedTask);
+      var keyGenerator = Substitute.For<IKeyGenerator>();
+      keyGenerator.GenerateKey().Returns(Task.FromResult("Ticket"));
+      var database = Substitute.For<IDatabaseRepository>();
+      database.AddAgniKaiAsync(Arg.Any<AgniKai>()).Returns(Task.CompletedTask);
+      var agniKaiRepo = new AgniKaiRepository(vault, keyGenerator, database);
+      var controller = new AgniKaiController(agniKaiRepo);
+
+      //Act
+      var action = await controller.Initiate();
+      var initiateResponse = action as OkNegotiatedContentResult<string>;
+
+      //Assert
+      Assert.IsNotNull(initiateResponse);
+      Assert.AreEqual(initiateResponse.Content, "Ticket");
+      await keyGenerator.Received(1).GenerateKey();
+      await vault.Received(1).AddAgniKaiTicket("Ticket");
+      //await database.Received(1).AddAgniKaiAsync(new AgniKai { Ticket = "Ticket" });
+      await database.Received(1).AddAgniKaiAsync(Arg.Any<AgniKai>());
+    }
+  }
+}
