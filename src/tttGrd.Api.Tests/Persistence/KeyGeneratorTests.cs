d9d16a686936c4a4537b14ec564193b8e015b9e1
ayaovi <djayespoiro@gmail.com>
2017-11-07 08:16:13 +0200

    Add key generator test.

---
+++ src/tttGrd.Api.Tests/Persistence/KeyGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using tttGrd.Api.Persistence;
+
+namespace tttGrd.Api.Tests.Persistence
+{
+  [TestFixture]
+  internal class KeyGeneratorTests
+  {
+    [Test]
+    public async Task GenerateGameTokenAsync_GivenUsername_ExpectGameToken()
+    {
+      //Arrange
+      var generator = new KeyGenerator();
+
+      //Act
+      var token = await generator.GenerateGameTokenAsync("Player-1");
+
+      //Assert
+      Assert.NotNull(token);
+    }
+  }
+}
