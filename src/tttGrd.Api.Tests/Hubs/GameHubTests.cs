da0375c47bd9ac07af35812670d6b58a2c369324
ayaovi <djayespoiro@gmail.com>
2017-11-10 11:00:01 +0200

    Add some GameHub tests.

--- src/tttGrd.Api.Tests/Hubs/GameHubTests.cs
+++ src/tttGrd.Api.Tests/Hubs/GameHubTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
 using NSubstitute;
 using NUnit.Framework;
 using tttGrd.Api.Hubs;
@@ -12,15 +13,53 @@ namespace tttGrd.Api.Tests.Hubs
   internal class GameHubTests
   {
     [Test]
-    public void Announce_GivenUsername_ExpectPlayerBeAdded()
+    public async Task Announce_GivenUsername_ExpectPlayerBeAdded()
     {
       //Arrange
       var mockDatabase = Substitute.For<IDatabaseRepository>();
       mockDatabase.AddConnectionAsync(Arg.Any<string>(), Arg.Any<string>()).Returns(Task.CompletedTask);
+      var context = Substitute.For<HubCallerContext>();
+      context.ConnectionId.Returns("1234");
+      var hub = new GameHub(mockDatabase)
+      {
+        Context = context
+      };
+
+      //Act
+      await hub.AnnounceAsync("Player-1");
+
+      //Assert
+      await mockDatabase.Received(1).AddConnectionAsync("Player-1", "1234");
+    }
+
+    [Test]
+    public async Task NotifyPlayerAsync_GivenParameters_ExpectPlayerBeNotified()
+    {
+      //Arrange
+      var mockDatabase = Substitute.For<IDatabaseRepository>();
+      mockDatabase.GetConnectionAsync(Arg.Any<string>()).Returns(Task.FromResult("1234"));
       var hub = new GameHub(mockDatabase);
 
-      //Act && Assert
-      Assert.ThrowsAsync<NullReferenceException>(() => hub.AnnounceAsync("Player-1"));
+      //Act
+      Assert.ThrowsAsync<NullReferenceException>(() => hub.NotifyPlayerAsync("Player-1", "Player-2"));
+
+      //Assert
+      await mockDatabase.Received(1).GetConnectionAsync("Player-1");
+    }
+
+    [Test]
+    public async Task NotifyOfChallengeAcceptedAsync_GivenParameters_ExpectPlayerBeNotified()
+    {
+      //Arrange
+      var mockDatabase = Substitute.For<IDatabaseRepository>();
+      mockDatabase.GetConnectionAsync(Arg.Any<string>()).Returns(Task.FromResult("1234"));
+      var hub = new GameHub(mockDatabase);
+
+      //Act
+      Assert.ThrowsAsync<NullReferenceException>(() => hub.NotifyPlayerAsync("Player-1", "Player-2"));
+
+      //Assert
+      await mockDatabase.Received(1).GetConnectionAsync("Player-1");
     }
 
     [Test]
@@ -52,10 +91,17 @@ namespace tttGrd.Api.Tests.Hubs
       mockDatabase.RecordMoveAsync(Arg.Any<string>(), Arg.Any<(int, int)>(), Arg.Any<Field>()).Returns(Task.CompletedTask);
       mockDatabase.GetStateAsync(Arg.Any<string>()).Returns(Task.FromResult(state));
       mockDatabase.GetAgniKaiByTicketAsync(Arg.Any<string>()).Returns(Task.FromResult(agniKai));
+      //var clients = Substitute.For<IHubCallerConnectionContext<object>>();
+      //clients.Group(Arg.Any<string>()).broadcastState(Arg.Any<State>()).Returns();
       var hub = new GameHub(mockDatabase);
+      //var hub = new GameHub(mockDatabase)
+      //{
+      //  Clients = clients,
+      //};
 
       //Act
       Assert.ThrowsAsync<NullReferenceException>(() => hub.SendMoveAI("Ticket", 0, 4, 'x'));
+      //await hub.SendMoveAI("Ticket", 0, 4, 'x');
 
       //Assert
       await mockDatabase.Received(1).RecordMoveAsync("Ticket", (0, 4), Field.X);
